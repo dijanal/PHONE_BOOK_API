083d6ba8d56b4e15d7f13d2d634bf6861d342a07
dijanal <dijanans5@hotmail.com>
2018-06-21 02:21:02 +0200

    Make api for post

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:'); // database will be stored in RAM only
 db.serialize(function() {
-    db.run("CREATE TABLE IF NOT EXISTS phone_book (id INTEGER PRIMARY KEY, first_name TEXT,last_name TEXT, number TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS phone_book (id INTEGER PRIMARY KEY autoincrement, first_name TEXT,last_name TEXT, number TEXT)");
     db.run("INSERT INTO phone_book (first_name, last_name, number) VALUES (?, ?, ?)", "Bob", "Sinclar", "+381641234567");
     db.run("INSERT INTO phone_book (first_name, last_name, number) VALUES (?, ?, ?)", "Alice", "Wonderland", "+38163122333");
     db.run("INSERT INTO phone_book (first_name, last_name, number) VALUES (?, ?, ?)", "Lee", "Bruce", "+381628000111");
@@ -32,6 +32,35 @@ db.all("SELECT * FROM phone_book ",  function(err, rows){
         res.json( rows );
   });
 })
+
+//post request
+
+const bodyParser = require('body-parser');
+api.use(bodyParser.urlencoded({extended: false}));
+api.use(bodyParser.json());
+ // hook up with your app
+api.post('/phone_book/post', (req, res) => {
+  console.log(req.body);
+
+  db.run(
+    'INSERT INTO users_to_pets VALUES ($first_name, $last_name, $number)',
+    // parameters to SQL query:
+    {
+      $first_name: req.body.name,
+      $last_name: req.body.job,
+      $number: req.body.pet,
+    },
+    // callback function to run when the query finishes:
+    (err) => {
+      if (err) {
+        res.send({message: 'error'});
+      } else {
+        res.send({message: 'success'});
+      }
+    }
+  );
+});
+
 
 
 
